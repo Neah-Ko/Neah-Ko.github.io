5324ba40523287d7b9aa55901a899d5262af9e17
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-13 18:55:31 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Etienne JODRY</title>
+		<script type="text/javascript" src="Scripts/jquery.js"></script>
+		<script type="text/javascript" src="Scripts/manip.js"></script>
+		<link rel="stylesheet" type="text/css" href="Styles/style.css" />
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li><a>About</a></li>
+					<li><a>IT Projects</a></li>
+					<li><a>Abilities</a></li>
+					<li><a>Side Activities</a></li>
+					<li><a>Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+		<section></section>
+		<footer></footer>
+	</body>
+</html>
