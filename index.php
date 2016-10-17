29cdc0aa418e9bf250b001e631758be6a6e240e9
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-17 11:45:17 +0200

    fin

--- index.php
+++ index.php
@@ -3,23 +3,65 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Etienne JODRY</title>
-		<script type="text/javascript" src="Scripts/jquery.js"></script>
-		<script type="text/javascript" src="Scripts/manip.js"></script>
-		<link rel="stylesheet" type="text/css" href="Styles/style.css" />
+		<script type="text/javascript" src="scripts/jquery.js"></script>
+		<script type="text/javascript" src="scripts/jquery-ui.js"></script>
+		<script type="text/javascript" src="scripts/manip.js"></script>
+		<link rel="stylesheet" type="text/css" href="styles/style.css" />
 	</head>
 	<body>
 		<header>
-			<nav>
-				<ul>
-					<li><a>About</a></li>
-					<li><a>IT Projects</a></li>
-					<li><a>Abilities</a></li>
-					<li><a>Side Activities</a></li>
-					<li><a>Contact</a></li>
-				</ul>
-			</nav>
+			<div id="about">
+				<h1>Etienne JODRY</h1>
+				<p>
+					I'm an IT student.<br>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor blandit justo sed suscipit. Etiam sed lectus commodo, facilisis enim at, posuere arcu. Fusce sollicitudin odio id ante pellentesque, at convallis ex lobortis. Aenean auctor metus nunc. Vivamus a tincidunt erat. Aenean venenatis finibus tellus, non congue justo efficitur et. Sed imperdiet eros ac tempor dictum. Praesent pulvinar augue at libero malesuada molestie. Ut feugiat lacinia purus in convallis. Integer tempor sed est vel egestas. Nunc finibus, arcu nec malesuada pellentesque, sapien libero finibus urna, vel sagittis ipsum ante eu lectus. Sed sem velit, semper id arcu sit amet, scelerisque lobortis mauris. Pellentesque varius ante at ipsum accumsan, in fermentum eros euismod. Donec pretium et leo ultrices tristique. Nam eu facilisis massa, vitae fringilla massa. 
+				</p>
+
+			</div>
 		</header>
-		<section></section>
-		<footer></footer>
+
+		<nav>
+			<ul id="menu">
+				<li><a href="#about">About</a></li>
+				<li><a href="#projects">IT Projects</a></li>
+				<li><a href="#abilities">Abilities</a></li>
+				<li><a href="side_act">Side Activities</a></li>
+				<li><a href="contact">Contact</a></li>
+			</ul>
+		</nav>
+
+		<section>
+			<div id="projects">
+				<div id="proj_intro">
+				</div>
+				<div id="proj_portfolio">
+
+				</div>
+			</div>
+			<div id="abilities">
+				
+			</div>
+			<div id="side_act">
+				
+			</div>
+		</section>
+		<footer>
+			<div id="contact">
+				<h1>Contact</h1>
+				<div id="con_social">
+					<h2>Socials</h2>
+					<ul id="socials">
+						<li><img class="socials_logos" src="images/linkedin_logo.png"><a href="https://fr.linkedin.com/in/etienne-jodry-395ab510a">Linkedin</a></li>
+						<li><img id="toto" class="socials_logos" src="images/github_logo.png"><a href="https://github.com/Neah-Ko">GitHub</a></li>
+						<!--
+						https://github.com/Neah-Ko
+						-->
+					</ul>
+				</div>
+				<div id="con_infos">
+					<h2>Contact me</h2>
+				</div>
+			</div>
+		</footer>
 	</body>
 </html>
