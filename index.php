aeba0ab8737d3ed888eaf4ebb3c808684267e45b
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-17 17:47:43 +0200

    Json

--- index.php
+++ index.php
@@ -14,7 +14,11 @@
 				<h1>Etienne JODRY</h1>
 				<p>
 					I'm an IT student.<br>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor blandit justo sed suscipit. Etiam sed lectus commodo, facilisis enim at, posuere arcu. Fusce sollicitudin odio id ante pellentesque, at convallis ex lobortis. Aenean auctor metus nunc. Vivamus a tincidunt erat. Aenean venenatis finibus tellus, non congue justo efficitur et. Sed imperdiet eros ac tempor dictum. Praesent pulvinar augue at libero malesuada molestie. Ut feugiat lacinia purus in convallis. Integer tempor sed est vel egestas. Nunc finibus, arcu nec malesuada pellentesque, sapien libero finibus urna, vel sagittis ipsum ante eu lectus. Sed sem velit, semper id arcu sit amet, scelerisque lobortis mauris. Pellentesque varius ante at ipsum accumsan, in fermentum eros euismod. Donec pretium et leo ultrices tristique. Nam eu facilisis massa, vitae fringilla massa. 
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Integer auctor blandit justo sed suscipit. Etiam sed lectus commodo, facilisis enim at, posuere arcu.
+					Fusce sollicitudin odio id ante pellentesque, at convallis ex lobortis. Aenean auctor metus nunc.
+					Vivamus a tincidunt erat.
+					Aenean venenatis finibus tellus, non congue justo efficitur et. Sed imperdiet eros ac tempor dictum.
 				</p>
 
 			</div>
@@ -23,43 +27,51 @@
 		<nav>
 			<ul id="menu">
 				<li><a href="#about">About</a></li>
-				<li><a href="#projects">IT Projects</a></li>
-				<li><a href="#abilities">Abilities</a></li>
-				<li><a href="side_act">Side Activities</a></li>
-				<li><a href="contact">Contact</a></li>
+				<li class="border"><a href="#my_projects">IT Projects</a></li>
+				<li class="border"><a href="#abilities">Abilities</a></li>
+				<li class="border"><a href="#side_act">Side Activities</a></li>
+				<li class="border"><a href="#contact">Contact</a></li>
 			</ul>
 		</nav>
 
 		<section>
-			<div id="projects">
+			<div id="my_projects">
 				<div id="proj_intro">
+					Samples of my projects over the yers.
 				</div>
 				<div id="proj_portfolio">
-
+					<ul id="content_portfolio">
+					</ul>
 				</div>
 			</div>
+
 			<div id="abilities">
 				
+
 			</div>
+
 			<div id="side_act">
 				
 			</div>
 		</section>
+
 		<footer>
 			<div id="contact">
-				<h1>Contact</h1>
 				<div id="con_social">
 					<h2>Socials</h2>
 					<ul id="socials">
 						<li><img class="socials_logos" src="images/linkedin_logo.png"><a href="https://fr.linkedin.com/in/etienne-jodry-395ab510a">Linkedin</a></li>
-						<li><img id="toto" class="socials_logos" src="images/github_logo.png"><a href="https://github.com/Neah-Ko">GitHub</a></li>
-						<!--
-						https://github.com/Neah-Ko
-						-->
+						<li><img class="socials_logos" src="images/github_logo.png"><a href="https://github.com/Neah-Ko">GitHub</a></li>
+						<li><img class="socials_logos" src="images/deviantart_logo.png"><a href="http://lord-neah.deviantart.com/">DeviantArt</a></li>
+						<li><img class="socials_logos" src="images/senscritique_logo.png"><a href="http://www.senscritique.com/Neah-Ko/">SensCritique</a></li>
 					</ul>
 				</div>
 				<div id="con_infos">
 					<h2>Contact me</h2>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor blandit justo sed suscipit.
+					 Etiam sed lectus commodo, facilisis enim at, posuere arcu.
+					</p>
 				</div>
 			</div>
 		</footer>
