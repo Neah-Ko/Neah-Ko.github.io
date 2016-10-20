fe42026a1d0a3c1ea330cdb88b3fc445c14d0d5e
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-20 11:39:16 +0200

    comt

---
+++ scripts/manip.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+	var portfolio = [{
+		"picture":"images/test.jpg",
+		"title":"Iter mini robots",
+		"tags":"NXT Mindstorm",
+		"year":"2013",
+		"link":"1"
+	},
+	{
+		"picture":"images/test.jpg",
+		"title":"Automatic Biathlon targets ",
+		"tags":"Picaxe | App inventor",
+		"year":"2015",
+		"link":"2"
+	},
+	{
+		"picture":"images/test.jpg",
+		"title":"ISN section website",
+		"tags":"From scratch | Web",
+		"year":"2015",
+		"link":"3"
+	},
+	{
+		"picture":"images/test.jpg",
+		"title":"The404 Website",
+		"tags":"WordPress | Web",
+		"year":"2016",
+		"link":"4"
+	},
+	{
+		"picture":"images/test.jpg",
+		"title":"TripAdvisor",
+		"tags":"From scratch | Web",
+		"year":"2016",
+		"link":"5"
+
+	},
+	{   
+		"picture":"images/test.jpg",
+		"title":"Projet Nina",
+		"tags":"Debian | Daemon",
+		"year":"2017",
+		"link":"6"
+	}];
+	for (var i=portfolio.length-1 ; i >=0 ; i--) {
+		console.log(i);
+		var idproject = "project"+portfolio[i]["link"];
+		var project = "<a href='index.php?page="+portfolio[i]["link"]+"'><div class='projects' id='"+idproject+"'>";
+		project += "<img src='"+portfolio[i]["picture"]+"'>";
+		project += "<h3>"+portfolio[i]["title"]+"</h3>";
+		project += "<h4>"+portfolio[i]["tags"]+"</h4>";
+		project += "</div></a>";
+		$('#content_portfolio').append(project);
+		//$('#'+idproject).css("background-image","url('"+portfolio[i]["picture"]+"')");
+		//background-image: url("img_tree.png");
+	}
+});
